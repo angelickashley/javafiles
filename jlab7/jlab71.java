26ceae68b4b7aa5fcf449c1fc86583aa6d1943a5
Angelic Kyle Ashley <akaangelic.work@gmail.com>
2026-03-03 16:30:35 +0000

    Update jlab71.java

--- jlab7/jlab71.java
+++ jlab7/jlab71.java
@@ -1,1 +1,15 @@
-
+import java.util.Scanner;
+//Student Name: Angelic Kyle Ashley Batan
+//Student ID:	C00317625
+//Class B
+//Date:	3/3/2026
+/* method menu for the menu option, and a method for each conversion*/
+ 
+// Error:	 
+// Why:	
+public class jlab71
+  {
+  public static void main(String[] args)
+    {
+      Scanner sc = new Scanner(System.in)
+      S
