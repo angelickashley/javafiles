652343aceabfd498835511679f7d43329d269198
Angelic Kyle Ashley <akaangelic.work@gmail.com>
2026-03-10 15:59:34 +0000

    Update jlab71.java

--- jlab7/jlab71.java
+++ jlab7/jlab71.java
@@ -11,5 +11,13 @@ public class jlab71
   {
   public static void main(String[] args)
     {
-      Scanner sc = new Scanner(System.in)
-      S
+      int elements;
+      int[] numbers;
+      int iterations;
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter the number of elements: ");
+      elements = sc.nextInt();
+      number = mew int[elements];
+      for (iterations = 0; iterations < elements; iterations++)
+       {
+         System.out.print("Please enter ")
